179f3a2ad687dc7e3f50226207db382c6d8b6656
Fedor <feodor.a.v.03@gmail.com>
2025-04-29 09:46:55 +0300

    alembic

--- backend/bot/dialogs/__init__.py
+++ backend/bot/dialogs/__init__.py
@@ -1,7 +1,9 @@
 from aiogram_dialog import Dialog
 from bot.dialogs.main_menu.windows import (
-    admin_projects_menu_window, start_question_window, 
-    question_history_window
+    main_menu_window,
+    start_question_window,
+    question_history_window,
+    start_window
 )
 from bot.dialogs.main_menu.states import MainMenu
 
@@ -9,7 +11,8 @@ from bot.dialogs.main_menu.states import MainMenu
 def menu_dialogs():
     return [
         Dialog(
-            admin_projects_menu_window(),
+            start_window(),
+            main_menu_window(),
             start_question_window(),
             question_history_window(),
         )
--- backend/bot/handlers/__init__.py
+++ backend/bot/handlers/__init__.py
@@ -1,12 +1,12 @@
 from aiogram import Router
 from .start import router as start_router
-from .on_click import router as on_click_router
 from .reactions import router as reactions_router
+from .menu import router as menu_router
 
 router = Router(name="main_router")
 router.include_router(start_router)
-router.include_router(on_click_router)
 router.include_router(reactions_router)
+router.include_router(menu_router)
 
 
 def get_handlers_router() -> Router:
