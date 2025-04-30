dfb07e343e8a0fbb8bdff3d05a45c628c91fa350
Mr-Dryg <shirobockow.dim@yandex.ru>
2025-04-30 10:55:30 +0300

    fix ml service 2

--- ml_service/main.py
+++ ml_service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from main.service import agent
+from service.service import agent
 
 class Data(BaseModel):
     text: str
